314eeaa05235905aa5a9c9f700097e401a0ff291
Missaoui Yassine <yassine.missaoui@enis.tn>
2025-06-26 12:40:15 +0200

    Primary Button's creation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import PrimaryButton from './components/PrimaryButton';
 function App() {
   return (
     <div className="App">
@@ -9,14 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <PrimaryButton />
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
